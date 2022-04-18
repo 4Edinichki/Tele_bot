10a01a68f360d91311946154f987caa66192ed81
4Edinichki <101266439+4Edinichki@users.noreply.github.com>
2022-04-18 16:48:39 +0300

    Add files via upload

---
+++ config_tele_bot.py
@@ -0,0 +1,14 @@
+TOKEN = '5354884640:AAHowNfLyvis4VmfKLBTQAQei7M2BsIExUo'  # MyFirstCriptoBot #MyCriptoBot
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'терра': 'LUNA',
+    'солана': 'SOL',
+    'трон': 'TRX',
+    'догекоин': 'DOGE',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'юань': 'CNY'
+}
